4b73814a26a236e4f78df371f3b1389e57548ac9
Raquel Alvarado <zilpa.alvarado@unah.hn>
2024-12-27 12:04:52 -0600

    aprendiendo sobre JSX

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,14 @@
-export default  function Header(){
+export default function Header(){
+
+    const name = 'RAQUEL';
+    const total = 100;
 
     return(
-        <h1>Desde header</h1>
+        <>
+            <h1>Prueba</h1>
+            <p>Hola: {name}, total a pagar: {total}</p>
+        </>
+        
     )
 }
 
